959fa7198c9a0f0f40c649f49dcbaf3a119d8120
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-03 17:48:17 +0100

    Improved standard case for hom_mat

--- sC/hom_mat.m
+++ sC/hom_mat.m
@@ -1,13 +1,29 @@
-function out = hom_mat(rotm,disp)
+function out = hom_mat(rotm, disp)
 % HOM_MAT(rotm, disp) returns the homogeneous rototranslational matrix OUT
 %   related to the rotation matrix ROTM and the displacement DISP.
+%
+%   Allows arguments as ADvar instances.
 
-if isa(rotm, 'ADvar') || isa(disp, 'ADvar')
-    b = ADvar([0 0 0 1], [0 0 0 0]);
-else
-b = [0 0 0 1];
-end
+    % Manage ADvars
+    if isa(rotm, 'ADvar') || isa(disp, 'ADvar')
+        b = ADvar([0 0 0 1], [0 0 0 0]);
+        out = [rotm disp; b];
 
-out = [rotm disp; b];
+    else % standard case
+        % Check to make sure the dimensions of the arguments make sense
+        assert(isvector(pos), "pos is not a vector");
+        assert(ismatrix(Rot), "Rot is not a matrix");
+
+        n = max(size(pos));
+        
+        assert(n == 3, ...
+            "Array size is not acceptable; it must consist of 3 elements");
+        % TODO: substitute with isMatRot when available
+        assert(all(sizes(Rot) == [n, n]), ...
+            "Matrix and vector sizes are not consistent");
+
+        out = [[rotm,       disp];
+               [zeros(1,3),    1]];
+    end
             
 end
