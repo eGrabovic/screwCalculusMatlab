3b6acf7f423f2f6dee4e2d4e4869714aee9ced7d
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-08 08:29:23 +0100

    Update rigidInverse.m
    Exploited rigidInverse and RigidInverse relation
    Kept both for now
    rigidInverse manages ADvar

--- sC/rigidInverse.m
+++ sC/rigidInverse.m
@@ -1,17 +1,19 @@
 function GstInv = rigidInverse(Gst)
 % RIGIDINVERSE(Gst) computes the inverse GSTINV of a homogeneous matrix GST.
+%   Allows ADvar class Gst argument.
 
-%     if all(size(Gst) ~= [4,4])
-%         error('Homogenous transformation matrix requires to have 4x4 dimension')
-%     end
+    % Manage ADvar argument
     if isa(Gst, 'ADvar')
-        
         Der = rigidInverse(Gst.der);
         GstInv = ADvar(rigidInverse(Gst.val), Der);
-        return
-        
+        return 
     end
-                Rst = Gst(1:3,1:3);
-                dst = Gst(1:3,4);
-                GstInv = [Rst.',-Rst.'*dst; 0 0 0 Gst(4,4)];
+
+
+    assert(all(size(mat) == [4 4]), ...
+        "Provided matrix must be an ADvar instance or a 4x4 homogeneous matrix");
+
+    Rst = Gst(1:3, 1:3);
+    dst = Gst(1:3, 4);
+    GstInv = [Rst.', -Rst.'*dst; 0, 0, 0, Gst(4, 4)];
 end
