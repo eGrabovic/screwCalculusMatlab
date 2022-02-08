01139d93b8e3de29f5bc0762b5af0c1a6a43449a
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-08 08:19:28 +0100

    Improved hat inverse
    Exploited vec and SkewToAxis relation
    Kept both because vec() is twist-specific

--- SkewToAxis.m
+++ SkewToAxis.m
@@ -2,10 +2,8 @@ function vet = SkewToAxis(mat)
 % SKEWTOAXIS(mat) generates an axis VET from a skew symmetric matrix MAT
 
     % TODO: implement skewQ and use it here
-    if any(size(mat) ~= [3,3]) % and not skewQ
-        disp("Argument has wrong dimensions!");
-        return
-    end
+    assert(all(size(mat) == [3, 3]), ...
+        "Argument has wrong dimensions!"); % and not skewQ
 
     vet = [mat(3,2), mat(1,3), mat(2,1)];
 end
--- sC/vec.m
+++ sC/vec.m
@@ -1,23 +1,33 @@
 function X = vec(Tw)
-% VEC(Tw) transforms a hat form twist TW into the vec form twist X: R_4x4 -> R_6
-%
+% VEC(Tw) transforms a hat-form twist TW into the vec-form twist X.
+%   Allows for ADvar class TW argument.
 
-if isa(Tw, 'ADvar')
-    X = ADvar(vec(Tw.val), vec(Tw.der)); % recursively apply the function to the value and derivative parts separately
-    return
-end
-
-if size(Tw, 1) == 3
+    % Manage instances of ADvar
+    if isa(Tw, 'ADvar')
+        % Recursively apply the function to the value and derivative parts 
+        % separately
+        X = ADvar(vec(Tw.val), vec(Tw.der)); 
+        return
+    end
     
-    X(1) = Tw(3,2);
-    X(2) = Tw(1,3);
-    X(3) = Tw(2,1);
-    X = X.';
-    return
-end
+    % Case of a 3x3 matrix
+    if all(size(Tw) == [3, 3])
+        X(1) = Tw(3,2);
+        X(2) = Tw(1,3);
+        X(3) = Tw(2,1);
+        X = X.'; % To return a column vector
+        return
+    end
+    
+    % Final case: 4x4 homogeneous matrix
 
-w_hat = Tw(1:3,1:3);
-v = Tw(1:3,4);
-X = [v; vec(w_hat)];
+    assert(all(size(mat) == [4, 4]), ...
+        "Supplied argument must be an ADvar instance or a 3x3 or 4x4 matrix");
+
+    w_hat = Tw(1:3,1:3);
+    v = Tw(1:3,4);
+
+    % Return the whole twist
+    X = [v; vec(w_hat)]; 
 
 end
