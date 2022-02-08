916d9da640f11550bddc8ef82a4caab4c9f91691
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-08 07:59:03 +0100

    Improved inverse of homogeneous matrix
    Exploited relation between hom_mat_inv and RigidInverse
    Keeping both for now, will decide later what to do

--- RigidInverse.m
+++ RigidInverse.m
@@ -1,7 +1,6 @@
 function rig_inv = RigidInverse(mat)
 % RIGIDINVERSE(g) gives the inverse transformation RIG_INV of MAT
 
-    assert(ismatrix(mat), "Provided argument must be a matrix");
     assert(all(size(mat) == [4 4]), ...
         "Provided matrix must be a 4x4 homogeneous matrix");
 
--- sC/hom_mat_inv.m
+++ sC/hom_mat_inv.m
@@ -1,14 +1,12 @@
 function GstInv = hom_mat_inv(Gst)
 % HOM_MAT_INV(Gst) computes the inverse GSTINV of a homogeneous matrix GST.
 
-Rst = Gst(1:3,1:3);
-dst = Gst(1:3,4);
-GstInv = [Rst.',-Rst.'*dst;0,0,0,1];
+    assert(all(size(Gst) == [4 4]), ...
+        "Provided matrix must be a 4x4 homogeneous matrix");
 
-% for codegen with ADvar
-% Rstval = Gst.val(1:3,1:3);
-% Rstder = Gst.der(1:3,1:3);
-% dstval = Gst.val(1:3,4);
-% dstder = Gst.der(1:3,4);
-% GstInv = ADvar([Rstval.',-Rstval.'*dstval;0,0,0,1], [Rstder.', -Rstder.'*dstval-Rstval.'*dstder; 0, 0, 0, 0]);
+    Rst = Gst(1:3, 1:3);
+    dst = Gst(1:3, 4);
+
+    GstInv = [Rst.', -Rst.' * dst;
+              0, 0, 0, 1];
 end
