9077d5f92bff1e1c63cb379348fb31058b07945b
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-10 18:10:28 +0100

    Update axisAngleToQuat.m
    Added help
    Added some checks

--- sC/axisAngleToQuat.m
+++ sC/axisAngleToQuat.m
@@ -1,8 +1,18 @@
 function out = axisAngleToQuat(theta, n)
-% TODO
-out = zeros(4,1);
-out(1) = cos(theta/2);
-out(2) = sin(theta/2).*n(1);
-out(3) = sin(theta/2).*n(2);
-out(4) = sin(theta/2).*n(3);
+% AXISANGLETOQUAT returns the quaternion OUT associated with the rotation
+%   of angle THETA around the general axis N.
+
+    assert(isnumeric(theta), ...
+           "Only numeric angle values are supported.");
+    assert((max(size(n)) == 3) && (min(size(n)) == 1), ...
+           "You must provide a 3-element array for the axis.")
+
+    % Init
+    out = zeros(4,1);
+
+    % TODO: why element-wise product?
+    out(1) = cos(theta/2);
+    out(2) = sin(theta/2) .* n(1);
+    out(3) = sin(theta/2) .* n(2);
+    out(4) = sin(theta/2) .* n(3);
 end
