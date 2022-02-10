957dfd1a90764070d5fb48a90cc0bee1914b56d1
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-10 18:20:37 +0100

    Update rotToAxisAngle.m
    Added help
    Minor readability improvements

--- sC/rotToAxisAngle.m
+++ sC/rotToAxisAngle.m
@@ -1,7 +1,14 @@
 function [n, theta] = rotToAxisAngle(R)
-% TODO
+% ROTTOAXISANGLE(R) uses the rotation matrix R to return the pair 
+%   [N, THETA] where:
+%   - N is the axis around which the rotation takes place
+%   - THETA is the angle of rotation.
 
-    theta = acos((trace(R)-1)/2) ;
-    n = 1/(2*sin(theta)).*[R(3,2) - R(2,3); R(1,3) - R(3,1); R(2,1) - R(1,2)];
+    theta = acos((trace(R) - 1) / 2) ;
+
+    n = 1/(2*sin(theta)) .* ...
+        [R(3, 2) - R(2, 3);
+         R(1, 3) - R(3, 1);
+         R(2, 1) - R(1, 2)];
 
 end
