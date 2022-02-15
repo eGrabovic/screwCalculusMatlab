697ea60970e1d8c9e1e4af5acbc0868e26200f51
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-15 08:17:01 +0100

    Update unitTwist.m

--- sC/unitTwist.m
+++ sC/unitTwist.m
@@ -2,14 +2,16 @@ function uT = unitTwist(jointType, axis, q)
 % UNITTWIST(jointType, axis, q) creates a unit twist UT from 
 %   - the type of joint JOINTTYPE ('P' for a prismatic, 'R' for a rotoidal)
 %   - the joint axis AXIS
-%   - (optional) the distance Q from the origin in case of a 'R' joint
+%   - the distance Q from the origin in case of a 'R' joint
 
-if strcmpi(jointType,'P') == true
-    uT = [axis;0;0;0];
-end
-
-if strcmpi(jointType,'R') == true
-    uT = [-cross(axis,q);axis];
-end
+    % Prismatic joint
+    if strcmpi(jointType,'P') == true
+        uT = [axis; 0; 0; 0];
+    end
+    
+    % Rotoidal joint
+    if strcmpi(jointType,'R') == true
+        uT = [-cross(axis,q); axis];
+    end
 
 end
