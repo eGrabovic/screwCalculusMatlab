a78f7d65a085feb4a65463cb60ccf65d19abed0b
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-17 17:54:03 +0100

    Update RK4_integrator.m
    Minor readability improvements
    Draft of help

--- sC/RK4_integrator.m
+++ sC/RK4_integrator.m
@@ -1,19 +1,20 @@
 function out = RK4_integrator(x, u, xdot, dt, Nsteps)
-% TODO
+% RK4_INTEGRATOR(x, u, xdot, dt, Nsteps) is an implementation of the
+%   Range-Kutta method for numerical integrations.
 
-h = dt./Nsteps;
+% TODO: ask more details
 
-x_end = x;
-
-for i = 1:Nsteps
+    h = dt ./ Nsteps;
     
-    v_1 = xdot(x_end, u);
-    v_2 = xdot(x_end + 0.5.*h.*v_1, u);
-    v_3 = xdot(x_end + 0.5.*h.*v_2, u);
-    v_4 = xdot(x_end + v_3.*h, u);
-    x_end = x_end + (1/6).*(v_1 + 2.*v_2 + 2.*v_3 + v_4).*h;
-
-end
+    x_end = x;
     
-out = x_end;
+    for i = 1:Nsteps
+        v_1 = xdot(x_end, u);
+        v_2 = xdot(x_end + 0.5 .* h .* v_1, u);
+        v_3 = xdot(x_end + 0.5 .* h .* v_2, u);
+        v_4 = xdot(x_end + v_3 .* h, u);
+        x_end = x_end + (1/6) * (v_1 + 2 .* v_2 + 2 .* v_3 + v_4) .* h;
+    end
+        
+    out = x_end;
 end
