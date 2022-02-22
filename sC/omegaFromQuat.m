7855f8b69a45b4833457d010795f8d8d1ea5d6c1
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-22 18:08:45 +0100

    Updated angular velocity functions
    Added help
    Minor readability improvements

--- sC/omegaFromQuat.m
+++ sC/omegaFromQuat.m
@@ -1,8 +1,9 @@
 function out = omegaFromQuat(q, qdot)
-% TODO
+% OMEGAFROMQUAT(q, qdot) computes the angular velocity OUT given the
+%   quaternion Q and its derivative QDOT.
 
     q0 = q(1);
-    qvec = [q(2);q(3);q(4)];
+    qvec = [q(2); q(3); q(4)];
     
-    out = pinv(0.5.*[-qvec.'; -hat(qvec) + eye(3).*q0])*qdot;
+    out = pinv(0.5 .* [-qvec.'; -hat(qvec) + eye(3) .* q0]) * qdot;
 end
--- sC/omegaFromQuat2.m
+++ sC/omegaFromQuat2.m
@@ -1,9 +1,15 @@
 function out = omegaFromQuat2(Q, Qdot)
-% TODO
+% OMEGAFROMQUAT2(Q, Qdot) is an alternative algorithm to omegaFromQuat().
+
     R = quatToRot(Q);
     q = Q(2:4);
     q0 = Q(1);
-    Rdot = 2*hat(q)*Qdot(1) + (2*q0*eye(3) + 4*hat(q))*...
-        (hat([1;0;0]).*Qdot(2) + hat([0;1;0]).*Qdot(3) + hat([0;0;1]).*Qdot(4));
-    out = vec(Rdot*R.');
+
+    Rdot = 2 * hat(q) * Qdot(1) + ...
+           (2 * q0 * eye(3) + 4 * hat(q)) * ...
+           (hat([1; 0; 0]) .* Qdot(2) + ...
+                hat([0; 1; 0]) .* Qdot(3) + ...
+                hat([0; 0; 1]) .* Qdot(4));
+
+    out = vec(Rdot * R.');
 end
