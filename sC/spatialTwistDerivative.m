65e035455d1f8444f115f0359b827f96c15def06
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-02-22 18:09:02 +0100

    Update spatialTwistDerivative.m
    Added help
    Minor readability improvements

--- sC/spatialTwistDerivative.m
+++ sC/spatialTwistDerivative.m
@@ -1,6 +1,9 @@
 function out = spatialTwistDerivative(spatialJac, qdot, qdotdot)
-% TODO
+% SPATIALTWISTDERIVATIVE(jac, qdot) computes the derivate OUT of the 
+%   tewist of a robot described through the spatial Jacobian JAC w.r.t. 
+%   the joint variables derivate QDOT and QDOTDOT.
 
-out = spatialJac*qdotdot + spatialJacDerivative(spatialJac, qdot)*qdot;
+    out = spatialJac * qdotdot + ...
+          spatialJacDerivative(spatialJac, qdot) * qdot;
 
 end
