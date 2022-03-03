9a60956adcc6e171f3248ca8287cbc419ba18510
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-03-03 18:40:36 +0100

    DH folder
    Created adhoc folder for Denavit-Hartemberg functions

---
+++ Denavit-Hartemberg/FKRow.m
@@ -0,0 +1,19 @@
+function hom_FK = FKRow(table_row)
+% FKROW(table_row) computes the DH homogeneous matrix HOM_MAT relative to 
+%   the parameter list TABLE_ROW (row of Denavit-Hartemberg table).
+
+%   TODO: maybe revert the name to the "original" DH?
+
+    a = table_row(1);
+    alpha = table_row(2);
+    d = table_row(3);
+    theta = table_row(4);
+%     type = table_row(5);
+    
+    Tz = HomZ(0, [0, 0, d]);
+    Rz = HomZ(theta, [0, 0, 0]);
+    Tx = HomX(0, [a, 0, 0]);
+    Rx = HomX(alpha, [0, 0, 0]);
+    
+    hom_FK = Tz*Rz*Tx*Rx;
+end
